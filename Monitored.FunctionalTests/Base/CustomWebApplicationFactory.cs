004e36b0b2b3e0c6e27026da8a4685eec518256f
Peter Gregg <greggp1987@hotmail.com>
2023-03-18 11:00:11 +0000

    Add functional tests project

---
+++ Monitored.FunctionalTests/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Common;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Monitored.API.Data;
+
+namespace Monitored.FunctionalTests.Base
+{
+
+    public class CustomWebApplicationFactory<TProgram>
+        : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<MonitoredAPIDataContext>));
+
+                services.Remove(dbContextDescriptor);
+
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbConnection));
+
+                services.Remove(dbConnectionDescriptor);
+
+                // Create open SqlConnection so EF won't automatically close it.
+                services.AddSingleton<DbConnection>(container =>
+                {
+                    var connection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=monitoredapitest;Trusted_Connection=True;MultipleActiveResultSets=true");
+                    connection.Open();
+
+                    return connection;
+                });
+
+                services.AddDbContext<MonitoredAPIDataContext>((container, options) =>
+                {
+                    var connection = container.GetRequiredService<DbConnection>();
+                    options.UseSqlServer(connection);
+                });
+            });
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
---
+++ Monitored.FunctionalTests/WeatherForecastScenarios.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Monitored.FunctionalTests.Base;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Monitored.FunctionalTests
+{
+    public class WeatherForecastScenarios : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Program> _factory;
+        public WeatherForecastScenarios(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task Get_get_filtered_weather_forecasts_and_response_ok_status_code()
+        {
+            // Act
+            var response = await _client.GetAsync("WeatherForecast?take=10&skip=0");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+    }
+}
